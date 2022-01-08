59cc12ee761c1813b2caef22cf84ff77d4c016e0
Hannah Han <46936828+HannahHan3@users.noreply.github.com>
2022-01-07 21:50:00 -0500

    update OffByN constructor

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -2,13 +2,11 @@
 public class OffByN implements CharacterComparator {
     private int Number;
 
-    public void OffByN(int n){
+    public OffByN(int n){
         this.Number = n;
     }
 
     public boolean equalChars(char x, char y) {
         return (x == y + Number || y == x + Number);
     }
-
-
 }
